71cc4085c58a612b4d7e48dec53bcbb4893b12ee
Seena Christin <seenachristin@Seenas-MacBook-Air.local>
2023-06-30 15:23:17 +0530

    modified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header =()=>{
    return(
     <div className="px-[175px] h-20 inline-flex justify-between items-center gap-[717px] 
-    text-white text-sm font-bold absolute top-0 right-0">
+    text-white text-sm font-bold absolute top-0 ">
      <div>Logo</div>
      <nav>
       <ul className="flex justify-start gap-28">
